06a58cd15a6f0d17fe355d190b7e6d207111aff2
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-08-01 15:43:46 +0330

    cart updated

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -29,6 +29,8 @@ const cartSlice = createSlice({
 
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -53,5 +55,8 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
 
 // 'reselect
