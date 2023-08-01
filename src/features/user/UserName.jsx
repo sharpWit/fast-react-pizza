4849e9480fada1cf6f7f81badc79907ac1ba874a
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-08-01 04:24:59 +0330

    redux added

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from 'react-redux';
+
 function UserName() {
-  return <div className="hidden text-sm font-semibold md:block">Saeed</div>;
+  const userName = useSelector((state) => state.user.username);
+
+  if (!userName) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
 
 export default UserName;
