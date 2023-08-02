ddf33f0b79459270e8e62681b4ecec580dcf547c
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-08-02 18:18:09 +0330

    assets

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fast-react-pizz/',
+  base: '/fast-react-pizza/',
 });
